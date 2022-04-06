605e1a74a296c0034f72b505626671eff274cb7a
LAPTOP-N6HJER3M\mengchu <mengchu35@gmail.com>
2022-04-06 08:54:31 +0800

    [vuetify] employee data

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-app>
+    <v-app-bar app color="primary" dark>
+      <v-icon class="mr-2">mdi-account-box-multiple</v-icon>
+      <span>Employee Data</span>
+    </v-app-bar>
+
+    <v-main>
+      <InputForm/>
+      <DataTable/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import InputForm from './components/InputForm.vue'
+import DataTable from './components/DataTable.vue'
+
+export default {
+  name: 'App',
+
+  components: {
+    InputForm,
+    DataTable
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
